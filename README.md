9b7007982c08b09df483fb5313dda58b42aa795e
Ibrahim <ibs@pop-os.localdomain>
2023-06-01 17:50:01 +0100

    Encapsulated the loop in milestone_3.py into functions. Updated readme.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Hangman is a classic game in which a player thinks of a word and the other playe
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
 
 Currently the "milestone_2.py" file contains a list of 5 strings representing fruits. It then picks one at random using the random module. Then it asks the user for some input, and finally checks if the input is a string containing one character and is contained in the alphabet. I don't know what the rest of the files are supposed to do, but whatever.
+
+The file "milestone_3.py" randomly selects a fruit, asks the user for some input, then checks if the input is a valid letter then checks if the letter is in the fruit. This is done via two functions, check_guess() which checks if a given letter in a giver word, and ask_for_input(), which creates a loop that keeps sking the user for a valid input.
--- milestone_3.py
+++ milestone_3.py
@@ -6,20 +6,24 @@ word = random.choice(word_list)
 
 print(word)
 
-guess = ""
-
-while True:
-    guess = input("Guess a letter!")
-
-    if len(guess) == 1 and guess.isalpha():
-        print("That's a valid input!")
-        if guess in list(word.upper()) or guess in list(word.lower()):
-            print("Good guess! {guess} is in the word.")
-            break
-        else:
-            print(f"Sorry, {guess} is not in the word. Try again.")
+def check_guess(guess, word):
+    guess_in_lower_case = guess.lower()
+    if guess_in_lower_case in word.lower():
+        print(f"Good guess! {guess_in_lower_case} is in the word.")
+        return True
     else:
-        print("That's not a valid input!")
+        print(f"Sorry, {guess} is not in the word. Try again.")
+        return False
+        
 
+def ask_for_input():
+    while True:
+        guess = input("Guess a letter!")
+        if len(guess) == 1 and guess.isalpha():
+            print("That's a valid input!")
+            if check_guess(guess, word): #check_guess returns a truth value
+                break
+        else:
+            print("That's not a valid input!")
 
-
+ask_for_input()
